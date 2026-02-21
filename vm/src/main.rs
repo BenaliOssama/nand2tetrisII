b4cc9efd0f077c6c60bb99a28c44f2182667ec9c
obenali <oussama.benali@ump.ac.ma>
2026-02-21 22:27:42 +0000

    oops, no need to emulate the vm i guess

--- vm/src/main.rs
+++ vm/src/main.rs
@@ -1,14 +1,72 @@
-mod config;
+use std::fs::{read_to_string, File};
+use std::io::Write;
 
-use config::VmConfig;
+struct CodeWriter {
+    output: File,
+    label_counter: usize,
+}
+
+impl CodeWriter {
+    fn new(path: &str) -> Self {
+        Self {
+            output: File::create(path).unwrap(),
+            label_counter: 0,
+        }
+    }
+
+    fn write_line(&mut self, s: &str) {
+        writeln!(self.output, "{}", s).unwrap();
+    }
+
+    fn write_arithmetic(&mut self, cmd: &str) {
+        match cmd {
+            "add" => {
+                self.write_line("@SP");
+                self.write_line("AM=M-1");
+                self.write_line("D=M");
+                self.write_line("A=A-1");
+                self.write_line("M=M+D");
+            }
+            "sub" => {
+                self.write_line("@SP");
+                self.write_line("AM=M-1");
+                self.write_line("D=M");
+                self.write_line("A=A-1");
+                self.write_line("M=M-D");
+            }
+            _ => {}
+        }
+    }
+
+    fn write_push_constant(&mut self, value: &str) {
+        self.write_line(&format!("@{}", value));
+        self.write_line("D=A");
+        self.write_line("@SP");
+        self.write_line("A=M");
+        self.write_line("M=D");
+        self.write_line("@SP");
+        self.write_line("M=M+1");
+    }
+}
 
 fn main() {
-    let config = VmConfig::load("vm_config.toml");
+    let input = read_to_string("SimpleAdd.vm").unwrap();
+    let mut writer = CodeWriter::new("SimpleAdd.asm");
 
-    println!("{:#?}", config);
+    for line in input.lines() {
+        let line = line.trim();
+        if line.is_empty() { continue; }
 
-    // initialize RAM using config
-    let mut memory = vec![0i16; config.memory_size];
+        let parts: Vec<&str> = line.split_whitespace().collect();
 
-    let sp = config.stack_start;
+        match parts[0] {
+            "push" if parts[1] == "constant" => {
+                writer.write_push_constant(parts[2]);
+            }
+            "add" | "sub" => {
+                writer.write_arithmetic(parts[0]);
+            }
+            _ => {}
+        }
+    }
 }
