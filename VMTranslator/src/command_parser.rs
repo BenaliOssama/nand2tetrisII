14e9b636ed3b833fb711cdddc363e408a783e2e3
OUSSAMABENALI <oussama.benali@ump.ac.ma>
2026-03-13 22:28:05 +0000

    make the code runnable

--- VMTranslator/src/command_parser.rs
+++ VMTranslator/src/command_parser.rs
@@ -1,7 +1,5 @@
 use std::fmt::Debug;
 
-use std::fmt::Debug;
-
 #[derive(Debug)]
 pub enum Inst {
     Push(Segment, u16),
@@ -35,19 +33,24 @@ pub enum Segment {
 #[derive(Debug)]
 pub struct Cmd {
     pub inst: Inst,
-    pub segment: 
+    // pub segment:
 }
 
 impl Cmd {
     pub fn parse_command(s: &str) -> Option<Cmd> {
         use Inst::*;
+        use Segment::*;
         let components: Vec<&str> = s.trim().split_whitespace().collect();
         match components[0] {
             "push" => {
-                return Some(Cmd { inst: PUSH });
+                return Some(Cmd {
+                    inst: Push(Argument, 0),
+                });
             }
             "pop" => {
-                return Some(Cmd { inst: POP });
+                return Some(Cmd {
+                    inst: Pop(Argument, 0),
+                });
             }
             _ => None,
         }
