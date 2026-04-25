6e4d41642ec475d14e4f1da262bc807fd631ab7c
OUSSAMABENALI <oussama.benali@ump.ac.ma>
2026-04-25 15:09:49 +0100

    work on argument this that ... they follow the same pattern as local. constant is done, now i will girue out static temp and pointer

--- VMTranslator/src/code_writer.rs
+++ VMTranslator/src/code_writer.rs
@@ -18,18 +18,37 @@ impl CodeWriter {
     }
 
     pub fn write_line(&mut self, cmd: Cmd) -> std::io::Result<()> {
+        use crate::command_parser::{Inst, Segment};
+
         if self.emit_source_comments {
             self.file.write_all(cmd.comment.as_bytes())?;
             self.file.write_all(b"\n")?;
         }
 
-        let str = format!("
-            @{cmd.}
-            D=A
-            @SP
-            A=M
-            M=D
-                          ")
+        match cmd.inst {
+            Inst::Push(Segment::Constant, val) => {
+                let asm = format!("@{val}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n");
+                self.file.write_all(asm.as_bytes())?;
+            }
+            Inst::Push(Segment::Local, val) => {
+                let asm = format!("@LCL\nD=M\n@{val}\nA=A+D\nD=M\n@SP\nA=M\nM=D\n@SP\nD=M+1\nM=D\n");
+                self.file.write_all(asm.as_bytes())?;
+            }
+            Inst::Push(Segment::Argument, val) => {
+                let asm = format!("@ARG\nD=M\n@{val}\nA=A+D\nD=M\n@SP\nA=M\nM=D\n@SP\nD=M+1\nM=D\n");
+                self.file.write_all(asm.as_bytes())?;
+            }
+            Inst::Push(Segment::This, val) => {
+                let asm = format!("@THIS\nD=M\n@{val}\nA=A+D\nD=M\n@SP\nA=M\nM=D\n@SP\nD=M+1\nM=D\n");
+                self.file.write_all(asm.as_bytes())?;
+            }
+            Inst::Push(Segment::That, val) => {
+                let asm = format!("@THAT\nD=M\n@{val}\nA=A+D\nD=M\n@SP\nA=M\nM=D\n@SP\nD=M+1\nM=D\n");
+                self.file.write_all(asm.as_bytes())?;
+            }
+            _ => todo!("other instructions not yet implemented"),
+        }
+
         Ok(())
     }
 }
